3c375a00ffd89073fd47e35e65f33c905a85372b
Jacob Briscoe <41913083+jacob-briscoe@users.noreply.github.com>
2025-08-12 13:19:54 -0400

    update dependencies and Angular configuration for version 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, RouterModule],
+    imports: [RouterOutlet, RouterModule],
     templateUrl: './app.component.html'
 })
 export class AppComponent {}
