34b3d809f64fcf5066112717a2d1b6847e10c4bc
Jacob Briscoe <41913083+jacob-briscoe@users.noreply.github.com>
2025-08-12 13:33:33 -0400

    Merge pull request #6
    * refactor: remove zone.js dependency and add zoneless change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideZonelessChangeDetection,
+} from "@angular/core";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideLocalStorage } from './tokens/local-storage.token';
+import { routes } from "./app.routes";
+import { provideLocalStorage } from "./tokens/local-storage.token";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     provideLocalStorage(),
   ],
